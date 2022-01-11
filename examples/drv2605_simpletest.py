fde1577336510a56de2a5cd125ca24fce2626d77
Alec Delaney <tekktrik@gmail.com>
2022-01-11 14:24:02 -0500

    Update example file to mention using all 8 slots

--- examples/drv2605_simpletest.py
+++ examples/drv2605_simpletest.py
@@ -22,9 +22,9 @@ effect_id = 1
 while True:
     print("Playing effect #{0}".format(effect_id))
     drv.sequence[0] = adafruit_drv2605.Effect(effect_id)  # Set the effect on slot 0.
-    # You can assign effects to up to 7 different slots to combine
+    # You can assign effects to up to 8 different slots to combine
     # them in interesting ways. Index the sequence property with a
-    # slot number 0 to 6.
+    # slot number 0 to 7.
     # Optionally, you can assign a pause to a slot. E.g.
     # drv.sequence[1] = adafruit_drv2605.Pause(0.5)  # Pause for half a second
     drv.play()  # play the effect
